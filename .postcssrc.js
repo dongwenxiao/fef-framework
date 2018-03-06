ba44a0932b6e451fd30a6f2dd01b297b0b7aa01f
dongwenxiao <cs_victor@126.com>
2018-03-06 17:52:54 +0800

    update

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,8 +1,14 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-   plugins: {
-      // to edit target browsers: use "browserslist" field in package.json
-      autoprefixer: {}
-   }
-};
+    plugins: [
+        require('autoprefixer')
+    ],
+    // 配置autoprefix
+    browsers: [
+        "iOS >= 8",
+        "Firefox >= 20",
+        "Android > 4.4",
+        "ie >= 9"
+    ]
+}
