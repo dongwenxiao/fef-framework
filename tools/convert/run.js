f8b5cf05458166b9d2189162cc1cbaf98f18c839
dongwenxiao <cs_victor@126.com>
2018-03-15 19:32:43 +0800

    update

--- tools/convert/run.js
+++ tools/convert/run.js
@@ -1,11 +1,14 @@
-// const write = require('./write')
+// config
 const appConfig = require('../../ini/app')
+
+// file factory
 const pageFactory = require('./factory/page')
 const containerFactory = require('./factory/container')
 const actionTypesFactory = require('./factory/action-types')
 const actionsFactory = require('./factory/actions')
 const reducerFactory = require('./factory/reducer')
 const initialStateFactoryFactory = require('./factory/initial-state-factory')
+const autoActionTypes = require('./factory/auto-action-types')
 
 // console.log(JSON.stringify(appConfig))
 
@@ -13,12 +16,12 @@ appConfig.pages.forEach(page => {
     // page
     pageFactory.make(page.name)
     containerFactory.make(page.name)
+
     // redux
     actionTypesFactory.make(page.name)
     actionsFactory.make(page.name)
     reducerFactory.make(page.name)
+    autoActionTypes.make([page.name])
     // initialStateFactoryFactory.make(page.name, page.config.state) // state 需要改配置结构
+    
 })
-
-
-// write('/Users/dongwenxiao/Documents/work_my/convert-config-2-code/project/apps/erp/client/ui/layout/pages/auto/goods.jsx', pageContent)
