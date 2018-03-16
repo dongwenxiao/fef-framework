e8fd2513e053a69a9c2bef82d089130b6aa61bfb
dongwenxiao <cs_victor@126.com>
2018-03-16 11:45:46 +0800

    update

--- tools/convert/utils/write.js
+++ tools/convert/utils/write.js
@@ -1,13 +1,10 @@
-// const config = require('./config')
+const config = require('../config')
 const fs = require('fs-extra')
-
 
 module.exports = function(file, content) {
 
-    // const file = config.root + '/file.txt'
-
     fs.outputFile(file, content)
-        .then(() => console.log('>>>>>>>> Finish: ' + file))
+        .then(() => console.log('>>>>>>>> Finish: ' + file.replace(config.root, '')))
         // .then(() => fs.readFile(file, 'utf8'))
         // .then(data => {
         //     console.log(data)
